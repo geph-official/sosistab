2f845c637514c288f2e87b2b518eab532172a488
nullchinchilla <nullchinchilla@pm.me>
2021-08-06 15:57:10 -0500

    increase a buffer

--- src/mux/multiplex_actor.rs
+++ src/mux/multiplex_actor.rs
@@ -22,7 +22,7 @@ pub async fn multiplex(
     conn_accept_send: Sender<RelConn>,
 ) -> anyhow::Result<()> {
     let conn_tab = Arc::new(ConnTable::default());
-    let (glob_send, glob_recv) = smol::channel::bounded(100);
+    let (glob_send, glob_recv) = smol::channel::bounded(1000);
     let (dead_send, dead_recv) = smol::channel::unbounded();
 
     // Reap death
