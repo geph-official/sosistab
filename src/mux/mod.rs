b118d8d0b3f5d5bdcf1bf0dc037f518c779f3816
nullchinchilla <nullchinchilla@pm.me>
2022-07-05 21:48:26 -0500

    limit receive memory usage

--- src/mux/mod.rs
+++ src/mux/mod.rs
@@ -27,8 +27,8 @@ impl Multiplex {
     /// Creates a new multiplexed session
     pub fn new(session: Session) -> Self {
         let (send_session, recv_session) = smol::channel::unbounded();
-        let (urel_send, urel_send_recv) = smol::channel::bounded(1024);
-        let (urel_recv_send, urel_recv) = smol::channel::unbounded();
+        let (urel_send, urel_send_recv) = smol::channel::bounded(256);
+        let (urel_recv_send, urel_recv) = smol::channel::bounded(256);
         let (conn_open, conn_open_recv) = smol::channel::unbounded();
         let (conn_accept_send, conn_accept) = smol::channel::bounded(100);
         let session = Arc::new(session);
